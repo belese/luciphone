14e8e7f2837a125b0838478cff4f382a3cb8f127
belese <besme2@gmail.com>
2015-08-08 15:43:37 +0000

    Add NfcMonitor

--- resources/NFCmonitor.py
+++ resources/NFCmonitor.py
@@ -1,2 +1,47 @@
-from py532 import PN532_I2C
+import time
 
+from py532lib.i2c import *
+from py532lib.frame import *
+from py532lib.constants import *
+
+class NFCmonitor :
+   def __init__(self) :
+       self.cardIn = False       
+       self.UUID = []
+       self.stopped = False
+       self.cbcardin = None
+       self.cbcardout = None
+       
+       #Initialise NFC_reader
+       self.pn532 = Pn532_i2c()
+       self.pn532.SAMconfigure()
+       
+   def registerCB(self,cbcardin = None,cbcardout = None):
+       self.cbcardin = cbcardin
+       self.cbcardout = cbcardout
+   
+   def _trust_uid(self,uid) :
+       return uid == self.pn532.get_uid() and uid == self.pn532.get_uid()
+   
+   def start(self) :       
+       print ("NFC Monitor started")
+       while not self.stopped :           
+           uid = self.pn532.get_uid()
+           if uid == self.UUID :             
+             time.sleep(0.2)                                     
+           elif uid and self._trust_uid(uid) :                
+                print ("New Card Detected",uid)
+                self.UUID = uid
+                if not self.cardIn :
+                    self.cardIn = True        
+                if self.cbcardin : self.cbcardin(self.UUID)
+                
+           elif not uid and self.cardIn and self._trust_uid(uid):                              
+                  print ("Card Removed")
+                  self.UUID = None
+                  self.cardIn = False                  
+                  if self.cbcardout : self.cbcardout(self.UUID)
+           
+                     
+a = NFCmonitor()
+a.start()
